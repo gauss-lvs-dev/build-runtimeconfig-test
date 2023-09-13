b917f10e284c3f398ab086f2ab4fa0950a838f11
pb <p.baumann@gauss-lvs.de>
2023-09-13 10:08:10 +0200

    Initial commit

---
+++ Check/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildTest
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Checking {0}!", args);
+    }
+  }
+}
---
+++ Lib2/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lib2
+{
+  public class Class2
+  {
+    public void DoAnything()
+    {
+      Console.WriteLine("Doing anyting...");
+    }
+  }
+}
